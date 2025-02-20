2a56f5a0509b3ca397fb3559cd5adc05987d49c1
Ishan Deshpande <ishangreat01@gmail.com>
2025-02-21 00:56:12 +0530

    Initial Commit

---
+++ url-shortener-master/server.js
@@ -0,0 +1,49 @@
+// filepath: /c:/Users/ishan/url-shortener/server.js
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mongoDbUrl = process.env.MONGO_DB_URL;
+if (!mongoDbUrl) {
+  console.error('MONGO_DB_URL is not defined in the environment variables');
+  process.exit(1);
+}
+
+mongoose.connect(mongoDbUrl, {
+  useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+
+  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 5000}`);
+});
