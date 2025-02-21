2170476070ef189525fa4863cd5cb0682be50961
Ishan Deshpande <ishangreat01@gmail.com>
2025-02-21 13:42:06 +0530

    Added Error Handling

--- url-shortener-master/server.js
+++ url-shortener-master/server.js
@@ -23,23 +23,38 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', async (req, res) => {
-  const shortUrls = await ShortUrl.find();
-  res.render('index', { shortUrls: shortUrls });
+  try {
+    const shortUrls = await ShortUrl.find();
+    res.render('index', { shortUrls: shortUrls });
+  } catch (error) {
+    console.error('Error fetching short URLs:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.post('/shortUrls', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl });
-  res.redirect('/');
+  try {
+    await ShortUrl.create({ full: req.body.fullUrl });
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error creating short URL:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.get('/:shortUrl', async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
-  if (shortUrl == null) return res.sendStatus(404);
+  try {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+    if (shortUrl == null) return res.sendStatus(404);
 
-  shortUrl.clicks++;
-  shortUrl.save();
+    shortUrl.clicks++;
+    shortUrl.save();
 
-  res.redirect(shortUrl.full);
+    res.redirect(shortUrl.full);
+  } catch (error) {
+    console.error('Error handling short URL:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.listen(process.env.PORT || 5000, () => {
